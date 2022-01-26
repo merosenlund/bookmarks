1195203e508841ac668eee9881c576b001c6ac5a
Mitch Rosenlund <merosenlund@gmail.com>
2022-01-26 13:42:56 -0800

    feat: Add account creation

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register")
     # # previous login view
     # # path('login/', views.user_login, name='login')
     # path("login/", auth_views.LoginView.as_view(), name="login"),
